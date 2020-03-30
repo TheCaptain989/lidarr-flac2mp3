ea2a306b3293aa12992a07c3d684cec6ce9e6b22
TheCaptain989 <thecaptain989@protonmail.com>
2020-03-30 12:19:37 -0500

    Fixes (#17)
    * Synergy update
    
    -Updated the log timestamp format
    -Updated log format
    -Improved debug log handling
    -Reduced regular logging verbosity
    -Edited title string manipulation
    -Added script self-name
    
    * Added debug wrapper script
    
    * Update flac2mp3.sh
    
    Fixes #13 
    Fixes #16
    
    * Update README.md
    
    Updated debug language.
    Removed references to ID3 tagging
    Added Recycle Bin language

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -10,24 +10,58 @@
 #  awk
 
 # Exit codes:
-#  0 - success
+#  0 - success; or test
 #  1 - no tracks files specified on command line
 # 11 - success, but unable to access Lidarr API due to missing Artist ID
 # 12 - success, but unable to access Lidarr API due to missing config file
 
+SCRIPT=$(basename "$0")
 LIDARR_CONFIG=/config/config.xml
 LOG=/config/logs/flac2mp3.txt
 MAXLOGSIZE=1024000
 MAXLOG=4
+DEBUG=0
 TRACKS="$lidarr_addedtrackpaths"
 [ -z "$TRACKS" ] && TRACKS="$lidarr_trackfile_path"      # For other event type
+RECYCLEBIN=$(python -c "import sqlite3
+conSql = sqlite3.connect('/config/lidarr.db')
+cursorObj = conSql.cursor()
+cursorObj.execute('SELECT Value from Config WHERE Key=\"recyclebin\"')
+print(cursorObj.fetchone()[0])
+conSql.close()")
+
+function usage {
+  usage="
+$SCRIPT
+Audio conversion script designed for use with Bazarr
+
+Source: https://github.com/TheCaptain989/lidarr-flac2mp3
+
+Usage:
+  $0 [-d] [-b <bitrate>]
+
+Arguments:
+  bitrate       # output quality in bits per second (SI units)
+
+Options:
+  -d    # enable debug logging
+  -b    # set bitrate; default 320K
+
+Examples:
+  $SCRIPT -b 320k              # Output 320 kilobits per second MP3
+                                     (same as default behavior)
+  $SCRIPT -d -b 160k           # Enable debugging, and output quality
+                                     160 kilobits per second
+"
+  echo "$usage"
+}
 
 # Can still go over MAXLOG if read line is too long
 #  Must include whole function in subshell for read to work!
 function log {(
   while read
   do
-    echo $(date +"%F %T")\|"$REPLY" >>"$LOG"
+    echo $(date +"%y-%-m-%-d %H:%M:%S.%1N")\|"$REPLY" >>"$LOG"
     FILESIZE=`wc -c "$LOG" | cut -d' ' -f1`
     if [ $FILESIZE -gt $MAXLOGSIZE ]
     then
@@ -45,18 +79,17 @@ function log {(
 while getopts ":db:" opt; do
   case ${opt} in
     d ) # For debug purposes only
-      MSG="DEBUG: Enabling debug logging."
+      MSG="Debug|Enabling debug logging."
       echo "$MSG" | log
       echo "$MSG"
-      ENVLOG=/config/logs/debugenv.txt
-      echo "--------$(date +"%F %T")--------" >>"$ENVLOG"
-      printenv | sort >>"$ENVLOG"
+      DEBUG=1
+      printenv | sort | sed 's/^/Debug|/' | log
       ;;
     b ) # Set bitrate
       BITRATE="$OPTARG"
       ;;
     : )
-      MSG="Invalid option: -$OPTARG requires an argument"
+      MSG="Error|Invalid option: -$OPTARG requires an argument"
       echo "$MSG" | log
       echo "$MSG"
       ;;
@@ -68,61 +101,54 @@ shift $((OPTIND -1))
 [ -z "$BITRATE" ] && BITRATE="320k"
 
 if [[ "$lidarr_eventtype" = "Test" ]]; then
-  echo "Lidarr event: $lidarr_eventtype" | log
-  echo "Script was test executed successfully." | log
+  echo "Info|Lidarr event: $lidarr_eventtype" | log
+  echo "Info|Script was test executed successfully." | log
   exit 0
 fi
 
 if [ -z "$TRACKS" ]; then
-  MSG="ERROR: No track file(s) specified! Not called from Lidarr?"
+  MSG="Error|No track file(s) specified! Not called from Lidarr?"
   echo "$MSG" | log
   echo "$MSG"
+  usage
   exit 1
 fi
 
 # Legacy one-liner script
 #find "$lidarr_artist_path" -name "*.flac" -exec bash -c 'ffmpeg -loglevel warning -i "{}" -y -acodec libmp3lame -b:a 320k "${0/.flac}.mp3" && rm "{}"' {} \;
 
-echo "Lidarr event: $lidarr_eventtype|Artist: $lidarr_artist_name|Artist ID: $lidarr_artist_id|Album ID: $lidarr_album_id|Tracks: $TRACKS" | log
-echo "Export bitrate: $BITRATE" | log
-echo "$TRACKS" | awk '
+echo "Info|Lidarr event: $lidarr_eventtype, Artist: $lidarr_artist_name ($lidarr_artist_id), Album: $lidarr_album_title ($lidarr_album_id), Export bitrate: $BITRATE, Tracks: $TRACKS" | log
+echo "$TRACKS" | awk -v Debug=$Debug -v Recycle="$RECYCLEBIN" -v Bitrate=$BITRATE '
 BEGIN {
   FFMpeg="/usr/bin/ffmpeg"
   FS="|"
   RS="|"
   IGNORECASE=1
-  Cover="/config/MediaCover/Albums/'$lidarr_album_id'/cover.jpg"
-  if (system("[ -f \""Cover"\" ]") == 0){
-    CoverCmds1="-i \""Cover"\" -map 1 "
-    CoverCmds2="-vcodec:v:1 copy -metadata:s:v title=\"Album cover\" -metadata:s:v comment=\"Cover (front)\" "
-  }
 }
 /\.flac/ {
   Track=$1
   sub(/\n/,"",Track)
   NewTrack=substr(Track, 1, length(Track)-5)".mp3"
-  print "Executing: "FFMpeg" -loglevel warning -i \""Track"\" "CoverCmds1"-map 0 -y -acodec libmp3lame -b:a '$BITRATE' -write_id3v1 1 -id3v2_version 3 "CoverCmds2"\""NewTrack"\""
-  Result=system(FFMpeg" -loglevel warning -i \""Track"\" "CoverCmds1"-map 0 -y -acodec libmp3lame -b:a '$BITRATE' -write_id3v1 1 -id3v2_version 3 "CoverCmds2"\""NewTrack"\" 2>&1")
+  print "Info|Writing: "NewTrack
+  if (Debug) print "Debug|Executing: "FFMpeg" -loglevel error -i \""Track"\" "CoverCmds1"-map 0 -y -acodec libmp3lame -b:a "Bitrate" -write_id3v1 1 -id3v2_version 3 "CoverCmds2"\""NewTrack"\""
+  Result=system(FFMpeg" -loglevel error -i \""Track"\" "CoverCmds1"-map 0 -y -acodec libmp3lame -b:a "Bitrate" -write_id3v1 1 -id3v2_version 3 "CoverCmds2"\""NewTrack"\" 2>&1")
   if (Result) {
-    print "ERROR: "Result" converting \""Track"\""
+    print "Error|"Result" converting \""Track"\""
   } else {
-    print "Deleting: \""Track"\""
-    system("[ -s \""NewTrack"\" ] && [ -f \""Track"\" ] && rm \""Track"\"")
+    if (Recycle=="") {
+      if (Debug) print "Debug|Deleting: \""Track"\""
+      system("[ -s \""NewTrack"\" ] && [ -f \""Track"\" ] && rm \""Track"\"")
+    } else {
+      match(Track,/^\/?[^\/]+\//)
+      RecPath=substr(Track,RSTART+RLENGTH)
+      sub(/[^\/]+$/,"",RecPath)
+      RecPath=Recycle RecPath
+      if (Debug) print "Debug|Moving: \""Track"\" to \""RecPath"\""
+      system("[ ! -e \""RecPath"\" ] && mkdir -p \""RecPath"\"; [ -s \""NewTrack"\" ] && [ -f \""Track"\" ] && mv -t \""RecPath"\" \""Track"\"")
+    }
   }
 }
-/\.mp3/ {
-  Track=$1
-  sub(/\n/,"",Track)
-  TmpTrack=substr(Track, 1, length(Track)-4)".tmp"
-  print "Executing: "FFMpeg" -loglevel warning -i \""Track"\" "CoverCmds1"-map 0 -y -acodec copy -write_id3v1 1 -id3v2_version 3 "CoverCmds2"-f mp3 \""TmpTrack"\""
-  Result=system(FFMpeg" -loglevel warning -i \""Track"\" "CoverCmds1"-map 0 -y -acodec copy -write_id3v1 1 -id3v2_version 3 "CoverCmds2"-f mp3 \""TmpTrack"\" 2>&1")
-  if (Result) {
-    print "ERROR: "Result" converting \""Track"\""
-  } else {
-    print "Deleting: \""Track"\" and Renaming: \""TmpTrack"\""
-    system("[ -s \""TmpTrack"\" ] && [ -f \""Track"\" ] && rm \""Track"\" && mv \""TmpTrack"\" \""Track"\"")
-  }
-}' | log
+' | log
 
 # Call Lidarr API to RescanArtist
 if [ ! -z "$lidarr_artist_id" ]; then
@@ -143,18 +169,18 @@ if [ ! -z "$lidarr_artist_id" ]; then
     
     [[ $BINDADDRESS = "*" ]] && BINDADDRESS=localhost
     
-    echo "Calling Lidarr API 'RefreshArtist' using artist id '$lidarr_artist_id' and URL 'http://$BINDADDRESS:$PORT$URLBASE/api/v1/command?apikey=$APIKEY'" | log
+    [ $DEBUG -eq 1 ] && echo "Debug|Calling Lidarr API 'RefreshArtist' using artist id '$lidarr_artist_id' and URL 'http://$BINDADDRESS:$PORT$URLBASE/api/v1/command?apikey=$APIKEY'" | log
     # Calling API
     RESULT=$(curl -s -d "{name: 'RefreshArtist', artistId: $lidarr_artist_id}" -H "Content-Type: application/json" \
       -X POST http://$BINDADDRESS:$PORT$URLBASE/api/v1/command?apikey=$APIKEY | jq -c '. | {JobId: .id, ArtistId: .body.artistId, Message: .status, DateStarted: .queued}')
-    echo "API returned: $RESULT" | log
+    [ $DEBUG -eq 1 ] && echo "Debug|API returned: $RESULT" | log
   else
-    echo "ERROR: Unable to locate Lidarr config file: '$LIDARR_CONFIG'" | log
+    echo "Warn|Unable to locate Lidarr config file: '$LIDARR_CONFIG'" | log
     exit 12
   fi
 else
-  echo "ERROR: Missing environment variable lidarr_artist_id" | log
+  echo "Warn|Missing environment variable lidarr_artist_id" | log
   exit 11
 fi
 
-echo "Done" | log
+echo "Info|Done" | log
