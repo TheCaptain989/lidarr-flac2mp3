ad38317cfc71be93d5c11964a5fa3333101f858c
TheCaptain989 <thecaptain989@protonmail.com>
2026-04-17 15:05:42 +0000

    chore: Update paulhatch/semantic-version refactor: Replaced eval with safer command-line argument processing function refactor: Implemented argument processing for advanced options refactor: Safe quoting implemented in echo commands

--- .github/tests/test_07_ffmpeg_params.sh
+++ .github/tests/test_07_ffmpeg_params.sh
@@ -12,25 +12,25 @@ setup_suite() {
 test_default_ffmpeg_params() {
   process_command_line
   set_ffmpeg_parameters
-  assert_equals "-c:v copy -map 0 -y -acodec libmp3lame -b:a 320k -write_id3v1 1 -id3v2_version 3" "$flac2mp3_ffmpeg_opts"
+  assert_equals "-c:v copy -map 0 -y -acodec libmp3lame -b:a 320k -write_id3v1 1 -id3v2_version 3" "${flac2mp3_ffmpeg_opts[*]}"
 }
 
 test_cbr_ffmpeg_params() {
   process_command_line -b 128k
   set_ffmpeg_parameters
-  assert_equals "-c:v copy -map 0 -y -acodec libmp3lame -b:a 128k -write_id3v1 1 -id3v2_version 3" "$flac2mp3_ffmpeg_opts"
+  assert_equals "-c:v copy -map 0 -y -acodec libmp3lame -b:a 128k -write_id3v1 1 -id3v2_version 3" "${flac2mp3_ffmpeg_opts[*]}"
 }
 
 test_vbr_ffmpeg_params() {
   process_command_line -v 0
   set_ffmpeg_parameters
-  assert_equals "-c:v copy -map 0 -y -acodec libmp3lame -q:a 0 -write_id3v1 1 -id3v2_version 3" "$flac2mp3_ffmpeg_opts"
+  assert_equals "-c:v copy -map 0 -y -acodec libmp3lame -q:a 0 -write_id3v1 1 -id3v2_version 3" "${flac2mp3_ffmpeg_opts[*]}"
 }
 
 test_advanced_ffmpeg_params() {
   process_command_line -a "-a:c aac -b:a 256k" -e aac
   set_ffmpeg_parameters
-  assert_equals "-a:c aac -b:a 256k" "$flac2mp3_ffmpeg_opts"
+  assert_equals "-a:c aac -b:a 256k" "${flac2mp3_ffmpeg_opts[*]}"
 }
 
 teardown() {
